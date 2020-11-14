8a4761f4942f94a9ace001b32e0b8ecc1613b4fa
shaelsien630 <shaelsien630@gmail.com>
2020-11-15 01:01:14 +0900

    community fragment 레이아웃 구현

---
+++ app/src/main/java/com/konkuk/select/adpater/CommunityFeedAdapter.kt
@@ -0,0 +1,36 @@
+package com.konkuk.select.adpater
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.konkuk.select.R
+import com.konkuk.select.model.Feed
+
+class CommunityFeedAdapter(val item: ArrayList<Feed>) : RecyclerView.Adapter<CommunityFeedAdapter.ItemHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommunityFeedAdapter.ItemHolder {
+        var v = LayoutInflater.from(parent.context).inflate(R.layout.community_feed_list, parent, false)
+        return ItemHolder(v)
+    }
+
+    inner class ItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var feedID: TextView = itemView.findViewById(R.id.feedID)
+        var feedUserTag: TextView = itemView.findViewById(R.id.feed_user_tag)
+        var feedImg: ImageView = itemView.findViewById(R.id.feed_image)
+        var heartCount : TextView = itemView.findViewById(R.id.hear_count_tv)
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+    override fun onBindViewHolder(holder: CommunityFeedAdapter.ItemHolder, position: Int) {
+        holder.feedID.text = item[position].id
+        holder.feedUserTag.text = item[position].tag
+//        holder.feedImg.setImageDrawable()
+        holder.heartCount.text = "좋아요 " + item[position].heartCount + "개"
+
+    }
+}
---
+++ app/src/main/java/com/konkuk/select/model/Feed.kt
@@ -0,0 +1,4 @@
+package com.konkuk.select.model
+
+data class Feed(val id: String, val tag: String, val img: String, val heartCount: Int) {
+}
